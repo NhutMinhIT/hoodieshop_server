a60d21cfa238a08119e1ac08098b0c3a6df9c7df
FPTnhutminh00 <nhutminh.fpt@gmail.com>
2023-03-01 11:16:29 +0700

    update models v4

--- models/product.js
+++ models/product.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const productSchema = mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        require: true,
     },
     description: {
         type: String,
